d551c6b0ed92be32c59c7f62be0d58b4669ccbdd
MirosBL45 <jovicmiroslav88@gmail.com>
2024-06-09 11:20:27 +0200

    5. assigment

--- app.js
+++ app.js
@@ -101,3 +101,44 @@ function multiply(a, b) {
 }
 
 console.log(multiply("123456789", "987654321"));
+console.log('####################----------------####################');
+
+
+console.log('5. assigment');
+console.log('Convert a string into an integer. The strings simply represent the numbers in words.');
+
+function parseInt(string) {
+    const numWords = {
+        'zero': 0, 'one': 1, 'two': 2, 'three': 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9, "ten": 10, "eleven": 11, "twelve": 12, "thirteen": 13, "fourteen": 14, "fifteen": 15, "sixteen": 16, "seventeen": 17, "eighteen": 18, "nineteen": 19, "twenty": 20, "thirty": 30, "forty": 40, "fifty": 50, "sixty": 60, "seventy": 70, "eighty": 80, "ninety": 90
+    };
+
+    const magnitudes = {
+        'hundred': 100,
+        'thousand': 1000,
+        'million': 1000000
+    };
+
+    const parts = string.replace(/-/, ' ').split(/[\s-]+/);
+
+    let result = 0;
+    let current = 0;
+
+    parts.forEach(part => {
+        if (numWords.hasOwnProperty(part)) {
+            current += numWords[part];
+        } else if (magnitudes.hasOwnProperty(part)) {
+            if (part === 'hundred' && current !== 0) {
+                current *= magnitudes[part];
+            } else {
+                current *= magnitudes[part];
+                result += current;
+                current = 0;
+            }
+        }
+    });
+
+    return result + current;
+};
+
+console.log(parseInt("seven hundred eighty-three thousand nine hundred and nineteen"));
+console.log('####################----------------####################');
